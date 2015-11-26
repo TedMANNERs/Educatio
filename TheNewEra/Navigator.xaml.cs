728da84438b4f3c7944a4fd6ff89bfe158acd7f7
TedMANNERs <mannersofted@gmail.com>
2015-11-26 12:52:11 +0100

    Added navigator user control

---
+++ TheNewEra/Navigator.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TheNewEra
+{
+    /// <summary>
+    /// Interaction logic for Navigator.xaml
+    /// </summary>
+    public partial class Navigator : UserControl
+    {
+        public Navigator()
+        {
+            InitializeComponent();
+            RootCanvas.DataContext = this;
+        }
+
+        public Rocket Rocket
+        {
+            get { return (Rocket)GetValue(RocketProperty); }
+            set { SetValue(RocketProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Rocket.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RocketProperty =
+            DependencyProperty.Register("Rocket", typeof(Rocket), typeof(Navigator), new PropertyMetadata(null));
+    }
+}
